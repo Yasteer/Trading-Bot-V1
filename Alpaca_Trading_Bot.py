44bdef286dbfe6dc70c1f4680bc21a8286748bc7
Yasteer Sewpersad <sewpersady@gmail.com>
2020-12-07 19:50:48 +0200

    Added API Login, put confidential info into seperate file.

--- Alpaca_Trading_Bot.py
+++ Alpaca_Trading_Bot.py
@@ -1,1 +1,8 @@
 import alpaca_trade_api as tradeapi 
+from Login_Information_Confidential import API_Key_ID,Secret_Key
+
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST(API_Key_ID,Secret_Key,alpaca_endpoint)
+account = api.get_account()
+
+print(account.status)
