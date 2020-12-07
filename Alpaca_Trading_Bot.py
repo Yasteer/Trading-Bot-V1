19e022edcbc16e8ec9c7e0fe283c5c9a36977ed2
Yasteer Sewpersad <sewpersady@gmail.com>
2020-12-07 20:03:00 +0200

    Check login status.

--- Alpaca_Trading_Bot.py
+++ Alpaca_Trading_Bot.py
@@ -2,7 +2,8 @@ import alpaca_trade_api as tradeapi
 from Login_Information_Confidential import API_Key_ID,Secret_Key
 
 alpaca_endpoint = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST(API_Key_ID,Secret_Key,alpaca_endpoint)
-account = api.get_account()
+api = tradeapi.REST(API_Key_ID,Secret_Key,alpaca_endpoint) # Send request over to Alpaca with my login information.
 
-print(account.status)
+# Checks if connection was succesful.
+account = api.get_account() 
+print(account.status) # Must say ACTIVE for successful connection.
