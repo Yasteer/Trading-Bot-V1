c1957bd294cd4e90dcba51ffe3ac3ae055b91650
Yasteer Sewpersad <sewpersady@gmail.com>
2020-12-08 20:50:06 +0200

    Moved brokerage details into class.

--- Alpaca_Trading_Bot.py
+++ Alpaca_Trading_Bot.py
@@ -1,9 +1,20 @@
 import alpaca_trade_api as tradeapi 
 from Login_Information_Confidential import API_Key_ID,Secret_Key
 
-alpaca_endpoint = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST(API_Key_ID,Secret_Key,alpaca_endpoint) # Send request over to Alpaca with my login information.
+# Alpaca Component 
+class Broker:
+    def __init__(self,API_Key,Secret_Key):
+        self.alpaca_endpoint = 'https://paper-api.alpaca.markets'
+        self.Key = API_Key
+        self.Secret_Key = Secret_Key
+        self.api = tradeapi.REST(self.Key, self.Secret_Key, self.alpaca_endpoint) # Send request over to Alpaca with my login information.
+        self.Stock ="IVV" # Currently bot only works on a single stock. 
 
-# Checks if connection was succesful.
-account = api.get_account() 
-print(account.status) # Must say ACTIVE for successful connection.
+    def LoginStatus(self):
+        account = self.api.get_account() 
+        print(account.status) # Must say ACTIVE for successful connection.
+        print("My name is Yasteera!")
+
+### Communicate order to Alpaca ###
+order = Broker(API_Key_ID,Secret_Key)
+order.LoginStatus()
