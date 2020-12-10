cef49918fb8c5dd7f8c9acdee148f3e92a5a44bb
Yasteer Sewpersad <sewpersady@gmail.com>
2020-12-10 21:21:42 +0200

    Changed sell functionality to prevent short positions.

--- Alpaca_Trading_Bot.py
+++ Alpaca_Trading_Bot.py
@@ -9,22 +9,29 @@ class Broker:
         self.Secret_Key = Secret_Key
         self.api = tradeapi.REST(self.Key, self.Secret_Key, self.alpaca_endpoint) # Send request over to Alpaca with my login information.
         self.Stock ="IVV" # Currently bot only works on a single stock. 
-        self.Quanity = 5
         self.PurchasePrice = 0
+        self.Current_Order = None
 
     def LoginStatus(self):
         account = self.api.get_account() 
         print(account.status) # Must say ACTIVE for successful connection.
 
-    def BuyStock(self, Stock, Quanity):
-        # Market order to make the purchase as soon as possible.
-        self.api.submit_order(self.Stock, self.Quanity, 'buy', 'market', 'day')
-        self.PurchasePrice = 0
+    def BuyStock(self, Quanity):
+        # Market order to make the purchase as soon as possible. # 
+        if self.Current_Order is None:
+            self.api.submit_order(self.Stock, Quanity, 'buy', 'market', 'day')
+        else
+            self.api.cancel_order(self.Current_Order.id)
 
-    def SellStock(self, Stock, Quanity):
-        # Limit order to only make a sale when a certain maximum/minimum limit is met.
-        SalePrice = self.PurchasePrice + self.PurchasePrice*0.1 # 10% Markup to cover costs.
-        self.api.submit_order(self.Stock, self.Quanity, 'sell', 'limit', 'opg', SalePrice)
+    def SellStock(self, Quanity):
+        # NB --> Setting all sales to have a long position (Don't want to obliterate myself just yet!) # 
+        # Alpaca allows the user to go short by selling a stock without first owning it. #
+        # As long the position > 0, we are going long. #
+
+        # Limit order to only make a sale when a certain maximum/minimum limit is met. #
+        if int(self.api.get_position(self.Stock).qty) > 0:
+            SalePrice = self.PurchasePrice + self.PurchasePrice*0.1 # 10% Markup to cover costs.
+            self.api.submit_order(self.Stock, Quanity, 'sell', 'limit', 'opg', SalePrice)
 
 ## Trading Algorithm ##
 
